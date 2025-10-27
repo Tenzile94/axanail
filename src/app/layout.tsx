12816433f0b2e5b0c154f2bada6f64328bb3986c
qasimovatenzile <qasimovatenzile@gmail.com>
2025-10-27 13:10:21 +0400

    ss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,9 +44,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Favicon */}
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/favicon-16x16.svg" sizes="16x16" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/favicon.svg" sizes="180x180" />
+        <link rel="icon" href="/favicon.png" type="image/png+xml" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png+xml" />
+        <link rel="apple-touch-icon" href="/favicon.png" sizes="180x180" />
         
         {/* Preconnect to optimize external resources */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
